67334f0848966e5483da6df76764537209e1154a
Aniket Sharma <as943798@gmail.com>
2026-02-24 15:13:47 +0530

    Create tests/sampletest.spec.ts

---
+++ tests/sampletest.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+// Sample Playwright test for EPAM homepage
+// Scenario:
+// 1) Navigate to https://www.epam.com/
+// 2) Maximize the window (use a large viewport size)
+// 3) Verify that the text "epam" is visible on the page
+
+test('EPAM homepage: should display "epam" text', async ({ page }) => {
+  // TODO: implement steps
+});
